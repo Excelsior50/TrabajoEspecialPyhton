ef93462357d740b08b8ce04775b58638e87dd6dd
Excelsior <ericchristensen59@gmail.com>
2024-04-10 22:01:14 -0300

    first commit

---
+++ js/general.js
@@ -0,0 +1,5 @@
+const buttonHamburguesa = document.querySelector(".hamburguesa")
+buttonHamburguesa.addEventListener("click", ()=>{
+    const menu = document.querySelector(".menuContainer")
+    menu.classList.toggle("--active")
+})
