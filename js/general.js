38403a4e24658094e2a9444a3ef029f5f632f2c6
Excelsior <ericchristensen59@gmail.com>
2024-04-18 12:41:03 -0300

    Update header and hamburg menu

--- js/general.js
+++ js/general.js
@@ -1,3 +1,6 @@
+
+//Menu Boton hamburguesa
+
 const buttonHamburguesa = document.querySelector(".hamburguesa")
 buttonHamburguesa.addEventListener("click", ()=>{
     const menu = document.querySelector(".menuContainer")
