f35e6a15dbb681b24231347333f49a1bbb80e7c5
Excelsior <ericchristensen59@gmail.com>
2024-05-06 10:21:18 -0300

    Carrousel, 3 img de prueba y responsive

--- js/general.js
+++ js/general.js
@@ -12,3 +12,49 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+
+//Carrousel Imagenes
+
+const grande = document.querySelector('.grande');
+const punto = document.querySelectorAll('.punto');
+
+//Recorrer TODOS los punto
+punto.forEach( (cadaPunto , i) => {
+    //Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click',()=>{
+
+        //Guardar pos de ese Punto
+        let posicion = i
+
+        //Calcular espacio desplazarse el Grande
+        //operacion = posicion * -33%
+        let operacion = posicion * -33
+
+        //Movemos el Grande
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        //Recorremos Todos los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            //Quitamos la class ACTIVO a todos los punto
+            punto[i].classList.remove('activo')
+        })
+        //Añadimos class ACTIVO en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://www.youtube.com/watch?v=2CEptqw-bSQ&ab_channel=EduardoFierro min13
